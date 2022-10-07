8b505105cf838fc16c48f1e5566a13bc52f492e3
ErikWohl <erik.y.wohlrab@gmail.com>
2022-10-07 08:13:54 +0200

    Added simple line draw feature.

--- src/main/java/at/fhtw/bic/ode_project/HelloApplication.java
+++ src/main/java/at/fhtw/bic/ode_project/HelloApplication.java
@@ -33,19 +33,57 @@ public class HelloApplication extends Application {
         var canvas = new Canvas(300, 300);
         root.getChildren().add(canvas);
 
+        var gc = canvas.getGraphicsContext2D();
+        gc.setLineWidth(1.0);
+
         var scene = new Scene(root, 300, 250, Color.WHITESMOKE);
 
+
+        //todo: https://docs.oracle.com/javafx/2/events/filters.htm
         scene.addEventFilter(MouseEvent.MOUSE_PRESSED, new EventHandler<MouseEvent>() {
             @Override
             public void handle(MouseEvent mouseEvent) {
+
+                gc.beginPath();
+
                 String msg =
                         "(x: "       + mouseEvent.getX()      + ", y: "       + mouseEvent.getY()       + ") --\n" +
                         "(sceneX: "  + mouseEvent.getSceneX() + ", sceneY: "  + mouseEvent.getSceneY()  + ") --\n" +
                         "(screenX: " + mouseEvent.getScreenX()+ ", screenY: " + mouseEvent.getScreenY() + ")";
-                System.out.printf("mouse click detected!\n%s\n", msg);
+                System.out.printf("mouse was pressed!\n%s\n", msg);
             }
         });
 
+        scene.addEventFilter(MouseEvent.MOUSE_DRAGGED, new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent mouseEvent) {
+
+
+                gc.lineTo(mouseEvent.getX(), mouseEvent.getY());
+                gc.stroke();
+                gc.moveTo(mouseEvent.getX(), mouseEvent.getY());
+
+                String msg =
+                        "(x: "       + mouseEvent.getX()      + ", y: "       + mouseEvent.getY()       + ") --\n" +
+                        "(sceneX: "  + mouseEvent.getSceneX() + ", sceneY: "  + mouseEvent.getSceneY()  + ") --\n" +
+                        "(screenX: " + mouseEvent.getScreenX()+ ", screenY: " + mouseEvent.getScreenY() + ")";
+                System.out.printf("mouse was dragged!\n%s\n", msg);
+            }
+        });
+
+        scene.addEventFilter(MouseEvent.MOUSE_RELEASED, new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent mouseEvent) {
+
+                gc.closePath();
+
+                String msg =
+                        "(x: "       + mouseEvent.getX()      + ", y: "       + mouseEvent.getY()       + ") --\n" +
+                        "(sceneX: "  + mouseEvent.getSceneX() + ", sceneY: "  + mouseEvent.getSceneY()  + ") --\n" +
+                        "(screenX: " + mouseEvent.getScreenX()+ ", screenY: " + mouseEvent.getScreenY() + ")";
+                System.out.printf("mouse was released!\n%s\n", msg);
+            }
+        });
         stage.setTitle("Lines");
         stage.setScene(scene);
         stage.show();
